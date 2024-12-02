039093b5b60500c1f6cae22645bc6e95f8376f4d
DayaPau <dayana.vallejos.casanova@udla.edu.ec>
2024-12-01 20:26:44 -0500

    Cambio de Flyout

--- BlocdeNotas/AppShell.xaml.cs
+++ BlocdeNotas/AppShell.xaml.cs
@@ -2,6 +2,13 @@
 {
     public partial class AppShell : Shell
     {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+        }
 
     }
 }
+
---
+++ BlocdeNotas/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace BlocdeNotas.Views;
+
+public partial class AboutPage : ContentPage
+{
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Models.About about)
+        {
+            // Navigate to the specified URL in the system browser.
+            await Launcher.Default.OpenAsync(about.MoreInfoUrl);
+        }
+    }
+
+}
