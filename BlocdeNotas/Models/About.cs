4e4583a24d5cecdda1ff376c0b9963b27b63dc6e
DayaPau <dayana.vallejos.casanova@udla.edu.ec>
2024-12-01 21:30:01 -0500

    Actualizaciones finales

--- BlocdeNotas/Models/About.cs
+++ BlocdeNotas/Models/About.cs
@@ -12,7 +12,7 @@ namespace BlocdeNotas.Models
         public string MoreInfoUrl => "https://aka.ms/maui";
         public string Message => "This app is written in XAML and C# with .NET MAUI.";
         public string Title { get; set; } = "Dayana Vallejos";
-        public string Description { get; set; } = "Hola, me gustan los gatos y lo filosofía de vida es CarpeDiem";
+        public string Description { get; set; } = "Hola, me gustan los gatos y mi filosofía de vida es CarpeDiem";
         public string ImagePath { get; set; } = "your_image.jpg"; 
     }
 }
