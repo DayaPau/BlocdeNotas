eaa90cd938f244eee94d2f8b93f09a0242322923
DayaPau <dayana.vallejos.casanova@udla.edu.ec>
2024-12-01 20:47:15 -0500

    Cambios Finales

--- BlocdeNotas/Models/About.cs
+++ BlocdeNotas/Models/About.cs
@@ -8,9 +8,11 @@ namespace BlocdeNotas.Models
 {
     internal class About
     {
-        public string Title => AppInfo.Name;
         public string Version => AppInfo.VersionString;
         public string MoreInfoUrl => "https://aka.ms/maui";
         public string Message => "This app is written in XAML and C# with .NET MAUI.";
+        public string Title { get; set; } = "Dayana Vallejos";
+        public string Description { get; set; } = "Hola, me gustan los gatos y lo filosofía de vida es CarpeDiem";
+        public string ImagePath { get; set; } = "your_image.jpg"; 
     }
 }
--- BlocdeNotas/Views/AboutPage.xaml.cs
+++ BlocdeNotas/Views/AboutPage.xaml.cs
@@ -1,7 +1,15 @@
+using BlocdeNotas.Models;
+
 namespace BlocdeNotas.Views;
 
 public partial class AboutPage : ContentPage
 {
+    public AboutPage()
+    {
+        InitializeComponent();
+        // Asegurando que el BindingContext esté correctamente asignado en el código-behind
+        this.BindingContext = new About();
+    }
     private async void LearnMore_Clicked(object sender, EventArgs e)
     {
         if (BindingContext is Models.About about)
